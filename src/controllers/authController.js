614babc83a4da38db662268087b9eca38a14c9ad
Roosevelt <roosevelt.vmj@gmail.com>
2026-09-20 14:39:32 -0300

    feat: implement jwt authentication, login generation and route protection

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -9,47 +9,45 @@ const authController = {
             const {email, password} = req.body
             const userExists = await userService.RetrieveUserEmail(email)
 
-            if(!userExists || userExists.lenght === 0){
+            if(!userExists || userExists.length === 0){
                 return res.status(400).json({
-                    message: "USUARIO NÃO ENCONTRADO"
+                    message: "UTILIZADOR NÃO ENCONTRADO"
                 })
             }
-
-            console.log(userExists);
-            
 
             const validPassword = await bcrypt.compare(password, userExists[0].password)
             if(!validPassword) {
-                res.status(401).json({
-                    message: "Senha invalida!!"
+                return res.status(401).json({
+                    message: "Palavra-passe inválida!!"
                 })
             }
-            const acessToken = jwt.sign(
+
+            const accessToken = jwt.sign(
                 {
-                id: userExists[0].id,
-                email: userExists[0].email,
-                name: userExists[0].name,
-                role: userExists[0].role
-            },
-            process.env.JWT_SECRET,
-            {
-                expiresIn: '15m'
-            }
-        )
-        res.status(200).json({
-            message:"LOGIN EFETUADO COM SUCESSO.",
-            token: acessToken
-        })
+                    id: userExists[0].id,
+                    email: userExists[0].email,
+                    name: userExists[0].name,
+                    role: userExists[0].role
+                },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: '15m'
+                }
+            )
+
+            return res.status(200).json({
+                message: "LOGIN EFETUADO COM SUCESSO.",
+                token: accessToken
+            })
         }
         catch(error){
             console.error(error)
             return res.status(500).json({
-                message:"OCORREU UM ERROR NO SERVIDOR",
+                message: "OCORREU UM ERRO NO SERVIDOR",
                 errorMessage: error.message
             })
         }
     }
 }
-
 
 export default authController
